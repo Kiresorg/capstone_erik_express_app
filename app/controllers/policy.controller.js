1869a23e627e3e23a5382e3f31a5e4ce0e5204d9
Erik Gross <erik.d.gross@gmail.com>
2020-10-21 21:12:45 -0700

    got CRUD working for Policies

--- app/controllers/policy.controller.js
+++ app/controllers/policy.controller.js
@@ -15,7 +15,7 @@ exports.create = (req, res) => {
         holder_first_name: req.body.holder_first_name,
         holder_last_name: req.body.holder_last_name,
         holder_account_id: req.body.holder_account_id,
-        is_active: req.body.is_active ? req.body.is_active : false,
+        is_active_policy: req.body.is_active_policy ? req.body.is_active_policy : false,
         has_active_claim: req.body.has_active_claim ? req.body.has_active_claim : false,
         effective_date: req.body.effective_date,
         termination_date: req.body.termination_date
@@ -31,6 +31,119 @@ exports.create = (req, res) => {
             res.status(500).send({
                 message:
                     err.message || "Error while creating Policy"
+            });
+        });
+};
+
+
+// get all policies
+exports.findAll = (req, res) => {
+    const last_name = req.query.holder_last_name;
+    var condition = last_name ? { holder_last_name: { $regex: new RegExp(last_name), $options: "i" }} : {}
+
+    Policy.find(condition)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Error while retreving Policies"
+            });
+        });
+};
+
+
+// get Policy by id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Policy.findById(id)
+        .then(data => {
+            if(!data)
+                res.status(404).send({ message: "Not found: Policy with id of " + id });
+            else res.send(data);
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({ message: "Error retreiving Policy with id of " + id });
+        });
+};
+
+// update Policy by id
+exports.update = (req, res) => {
+    if(!req.body) {
+        return res.status(400).send({
+            message: "Empty data for update"
+        });
+    }
+
+    const id = req.params.id;
+
+    Policy.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Unable to update Policy with id of ${id}.`
+                });
+            } else res.send({ message: "Successfully updated Policy." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error while trying to update Policy with id of " + id
+            });
+        });
+};
+
+// delete Policy by id
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Policy.findByIdAndRemove(id)
+        .then(data => {
+            if(!data) {
+                res.status(404).send({
+                    message: `Unable to delete Policy with id of ${id}.`
+                });
+            } else {
+                res.send({
+                    message: "Policy deleted successfully."
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Unable to delete Policy with id of " + id
+            });
+        });
+};
+
+// delete all Policies
+exports.deleteAll = (req, res) => {
+    Policy.deleteMany({})
+        .then(data => {
+            res.send({
+                message: `${data.deletedCount} Policies were deleted successfully.`
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "An error occurred while attempting to delete all Policies"
+            });
+        });
+};
+
+// get all active Policies
+exports.findAllActive = (req, res) => {
+    Policy.find({ is_active_policy: true })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "An error occurred while retrieving active Policies"
             });
         });
 };
--- app/routes/policy.routes.js
+++ app/routes/policy.routes.js
@@ -6,6 +6,23 @@ module.exports = app => {
     // create new Policy
     router.post("/", policies.create);
 
+    // get all Policies
+    router.get("/", policies.findAll);
+
+    // get all active Policies
+    router.get("/active", policies.findAllActive);
+
+    // get Policy by id
+    router.get("/:id", policies.findOne);
+
+    // update Policy by id
+    router.put("/:id", policies.update);
+
+    // delete Policy by id
+    router.delete("/:id", policies.delete);
+
+    // delete all Policies
+    router.delete("/", policies.deleteAll);
 
     app.use('/api/policies', router);
 };
