037bd913870e092967e2267b457ccd689335698c
Dominique Mendes <dom01708@tjx.com>
2021-02-12 15:39:40 -0500

    No Data Retrieved Error Message Changed

--- app/controllers/claim.controller.js
+++ app/controllers/claim.controller.js
@@ -78,7 +78,7 @@ exports.update = (req, res) => {
         .then(data => {
             if (!data) {
                 res.status(404).send({
-                    message: `Unable to update Claim with id of ${id}.`
+                    message: `Claim with id of ${id} was not found when attempting to edit.`
                 });
             } else res.send({ message: "Successfully updated Claim." });
         })
